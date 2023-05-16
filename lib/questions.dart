f96a61148511d902c0cba49cb990899e60054dcb
Simon Pierre Ndizihiwe <ndizihiwesimon@gmail.com>
2023-05-16 16:58:56 +0200

    dynamic questions

--- lib/questions.dart
+++ lib/questions.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_quiz/answer_button.dart';
+import 'package:flutter_quiz/data/questions.dart';
 
 class QuestionScreen extends StatefulWidget {
   const QuestionScreen({super.key});
@@ -10,8 +12,31 @@ class QuestionScreen extends StatefulWidget {
 }
 
 class _QuestionScreenState extends State<QuestionScreen> {
+  final currentQuestion = questions[0];
   @override
   Widget build(context) {
-    return const Text('Questions Screen');
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            currentQuestion.question,
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          ...currentQuestion.answers.map(
+            (answer) => AnswerButton(
+              answerText: answer,
+              onTap: () {},
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
