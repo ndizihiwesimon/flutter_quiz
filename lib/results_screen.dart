8bcc4317897aa9bd93b082904f924c73aa3dd1a6
Simon Pierre Ndizihiwe <ndizihiwesimon@gmail.com>
2023-05-19 15:59:38 +0200

    Styling results screen

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,24 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_quiz/data/questions.dart';
+import 'package:flutter_quiz/questions_summary.dart';
 
 class ResultScreen extends StatelessWidget {
   const ResultScreen({super.key, required this.chosenAnswers});
   final List<String> chosenAnswers;
+  List<Map<String, Object>> getSummaryData() {
+    final List<Map<String, Object>> summary = [];
+    for (var i = 0; i < chosenAnswers.length; i++) {
+      summary.add({
+        'question_id': i,
+        'question': questions[i].question,
+        'correct_answer': questions[i].answers[0],
+        'user_answer': chosenAnswers[i],
+      });
+    }
+    return summary;
+  }
 
   @override
   Widget build(BuildContext context) {
-    List<Map<String, Object>> getSummaryData() {
-      final List<Map<String, Object>> summary = [];
-      for (var i = 0; i < chosenAnswers.length; i++) {
-        summary.add({
-          'question_id': i,
-          'question': questions[i].question,
-          'correct_answer': questions[i].answers[0],
-          'user_answer': chosenAnswers[i],
-        });
-      }
-      return summary;
-    }
+    final summaryData = getSummaryData();
+    final numTotalQuestions = questions.length;
+    final numCorrectAnswers = summaryData.where((data) {
+      return data['correct_answer'] == data['user_answer'];
+    }).length;
 
     return SizedBox(
       width: double.infinity,
@@ -27,11 +33,18 @@ class ResultScreen extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text('Answers are....'),
+            Text(
+              'You answered $numCorrectAnswers  out of $numTotalQuestions questions correctly!',
+              style: const TextStyle(
+                  color: Color.fromARGB(255, 242, 241, 241),
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
+            ),
             const SizedBox(
               height: 30,
             ),
-            const Text('List of answers'),
+            QuestionsSummary(summaryData: getSummaryData()),
             const SizedBox(
               height: 30,
             ),
